4306c562156107c4cd3d6e2b02db4e0b29bda4ab
Robin Davies <robbiedaves@hotmail.com>
2017-04-23 13:24:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Java Examples
+
+A collection of Java examples
---
+++ lambda/src/main/java/com/robbiedaves/javaexamples/lambda/predicates/CheckTrait.java
@@ -0,0 +1,5 @@
+package com.robbiedaves.javaexamples.lambda.predicates;
+
+public interface CheckTrait {
+    boolean test(Animal a);
+}
